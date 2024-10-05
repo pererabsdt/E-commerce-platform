dec49e4771b882f0c4b8aa6bc9eeb7593672d1ae
Parawork <154836766+Parawork@users.noreply.github.com>
2024-10-05 20:52:34 +0530

    Checkout page organized and bug fixed

--- Frontend/src/pages/checkout/Checkout.js
+++ Frontend/src/pages/checkout/Checkout.js
@@ -13,14 +13,14 @@ import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import ChevronLeftRoundedIcon from '@mui/icons-material/ChevronLeftRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
-import AddressForm from './components/AddressForm';
-import getCheckoutTheme from './theme/getCheckoutTheme';
-import Info from './components/Info';
-import InfoMobile from './components/InfoMobile';
-import PaymentForm from './components/PaymentForm';
-import Review from './components/Review';
-import SitemarkIcon from './components/SitemarkIcon';
+import AddressForm from '../../components/AddressForm';
+import getCheckoutTheme from '../../theme/getCheckoutTheme';
+import Info from '../../components/Info';
+import InfoMobile from '../../components/InfoMobile';
+import PaymentForm from '../../components/PaymentForm';
+import Review from '../../components/Review';
 import TemplateFrame from './TemplateFrame';
+import { LogoIcon } from '../sign/CustomIcons';
 
 const steps = ['Shipping address', 'Payment details', 'Review your order'];
 function getStepContent(step) {
@@ -94,7 +94,7 @@ export default function Checkout() {
               gap: 4,
             }}
           >
-            <SitemarkIcon />
+            <LogoIcon />
             <Box
               sx={{
                 display: 'flex',
--- Frontend/src/pages/checkout/TemplateFrame.js
+++ Frontend/src/pages/checkout/TemplateFrame.js
@@ -1,17 +1,11 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
+
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
-import ToggleColorMode from './components/ToggleColorMode';
-import getCheckoutTheme from './theme/getCheckoutTheme';
+import getCheckoutTheme from '../../theme/getCheckoutTheme';
 
 const StyledAppBar = styled(AppBar)(({ theme }) => ({
   position: 'relative',
@@ -29,15 +23,10 @@ const StyledAppBar = styled(AppBar)(({ theme }) => ({
 }));
 
 function TemplateFrame({
-  showCustomTheme,
-  toggleCustomTheme,
   mode,
-  toggleColorMode,
   children,
 }) {
-  const handleChange = (event) => {
-    toggleCustomTheme(event.target.value === 'custom');
-  };
+  
   const checkoutTheme = createTheme(getCheckoutTheme(mode));
 
   return (
@@ -54,46 +43,6 @@ function TemplateFrame({
               p: '8px 12px',
             }}
           >
-            <Button
-              variant="text"
-              size="small"
-              aria-label="Back to templates"
-              startIcon={<ArrowBackRoundedIcon />}
-              component="a"
-              href="/material-ui/getting-started/templates/"
-              sx={{ display: { xs: 'none', sm: 'flex' } }}
-            >
-              Back to templates
-            </Button>
-            <IconButton
-              size="small"
-              aria-label="Back to templates"
-              component="a"
-              href="/material-ui/getting-started/templates/"
-              sx={{ display: { xs: 'auto', sm: 'none' } }}
-            >
-              <ArrowBackRoundedIcon />
-            </IconButton>
-            <Box sx={{ display: 'flex', gap: 1 }}>
-              <FormControl variant="outlined" sx={{ minWidth: 180 }}>
-                <Select
-                  size="small"
-                  labelId="theme-select-label"
-                  id="theme-select"
-                  value={showCustomTheme ? 'custom' : 'material'}
-                  onChange={handleChange}
-                  label="Design Language"
-                >
-                  <MenuItem value="custom">Custom Theme</MenuItem>
-                  <MenuItem value="material">Material Design 2</MenuItem>
-                </Select>
-              </FormControl>
-              <ToggleColorMode
-                data-screenshot="toggle-mode"
-                mode={mode}
-                toggleColorMode={toggleColorMode}
-              />
-            </Box>
           </Toolbar>
         </StyledAppBar>
         <Box sx={{ flex: '1 1', overflow: 'auto' }}>{children}</Box>
